0b17b47f3fb7fdaac633dfd9ca4fc4bcac129ef2
Brian Jones <mojobojo@gmail.com>
2016-05-12 21:01:21 +0900

    smarter fish handling for gpg-agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,7 @@ set -x GOPATH /Users/bojo/Development/go/standard
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
-if not pgrep -x gpg-agent > /dev/null
-  set pid (gpg-agent --enable-ssh-support --use-standard-socket --daemon | sed '3!d' | cut -d"=" -f2 | cut -d";" -f1)
-  set -x SSH_AGENT_PID $pid
-  set -x GPG_AGENT_INFO ~/.gnupg/S.gpg-agent:$pid:1
-  set -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
-end
+gnugpg
 
 set fish_greeting
 
