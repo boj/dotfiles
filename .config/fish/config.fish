d226c7040b0edddc3f88c850ff11d591b810bb68
Brian Jones <mojobojo@gmail.com>
2016-05-12 16:11:49 +0900

    gpg-agent ssh handling for yubikey

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,16 +4,20 @@ set -x GOPATH /Users/bojo/Development/go/standard
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
+if not pgrep -x gpg-agent > /dev/null
+  set pid (gpg-agent --enable-ssh-support --use-standard-socket --daemon | sed '3!d' | cut -d"=" -f2 | cut -d";" -f1)
+  set -x SSH_AGENT_PID $pid
+  set -x GPG_AGENT_INFO ~/.gnupg/S.gpg-agent:$pid:1
+  set -x SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
+end
+
 set fish_greeting
-
-alias ls "exa"
-
-keychain --eval --quiet -Q --agents "ssh,gpg" > /dev/null
 
 if status --is-interactive
     archey -c
-	cd
+    cd
 end
+
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
